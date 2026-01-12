86a9b50a2154de7c62221ad6d06941bc057b920a
MrHarsh002 <harshkumar26100@gmail.com>
2026-01-12 22:34:10 +0530

    change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://ayur-sutra-five.vercel.app/api",
-  baseURL: "http://localhost:5000/api",
   withCredentials: true,
 });
 
